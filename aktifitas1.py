e4ce0ea38dd025568a07d2c1377ce436ce17357c
pratiwisnd <79701554+pratiwisnd@users.noreply.github.com>
2021-02-26 20:00:41 +0700

    pratiwi sanada 1904060

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+nama=input("masukin nama Anda:")
+umur=20
+
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
+print("nama Anda: ", nama)
+print("umur Anda: ", umur)
+print("")
+
+
+
