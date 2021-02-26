e9b80cb69132748f6c54ed52ba2d7240e703f519
pratiwisnd <79701554+pratiwisnd@users.noreply.github.com>
2021-02-26 20:03:10 +0700

    pratiwii

---
+++ aktivitas3.py
@@ -0,0 +1,15 @@
+print("percabangan pada bahasa pemrograman Python")
+print("pilih bahasa pemrograman: ")
+print("1. Python")
+print("2. C")
+
+pilih=int(input("pilih menu (1/2): "))
+
+if pilih == 1:
+    print("bahasa pemrograman yang dipilih adalah python")
+elif pilih == 2:
+    print("bahasa pemrograman yang dipilih adalah C")
+else :
+    print("pilihan tidak tersedia!!!")
+    
+      
